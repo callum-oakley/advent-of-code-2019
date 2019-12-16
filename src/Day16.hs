5bff7604d213d8e1ca917f08c9a0ab9d1241fafd
Callum Oakley <c.oakley108@gmail.com>
2019-12-16 23:02:35 +0000

    day 16 part 2

--- src/Day16.hs
+++ src/Day16.hs
@@ -28,13 +28,27 @@ fft =
 part1' :: String -> String
 part1' = take 8 . fft
 
+stepLastHalf :: [Int] -> [Int]
+stepLastHalf = scanr1 (\x y -> abs (x + y) `rem` 10)
+
+fftLastHalf :: String -> String
+fftLastHalf =
+  foldr (:) "" .
+  fmap intToDigit . (!! 100) . iterate stepLastHalf . map digitToInt
+
 part2' :: String -> String
-part2' s = take 8 $ drop (read $ take 7 full) full
+part2' s = take 8 . fftLastHalf . drop offset . concat $ replicate 10000 s
   where
-    full = fft . concat $ replicate 10000 s
+    offset = read $ take 7 s
+
+input :: IO String
+input = takeWhile (/= '\n') <$> readFile "data/input16"
 
 part1 :: IO String
-part1 = part1' . takeWhile (/= '\n') <$> readFile "data/input16"
+part1 = part1' <$> input
+
+part2 :: IO String
+part2 = part2' <$> input
 
 test :: IO ()
 test = defaultMain tests
@@ -47,12 +61,14 @@ tests =
         part1' "80871224585914546619083218645595" @?= "24176176"
         part1' "19617804207202209144916044189917" @?= "73745418"
         part1' "69317163492948606335995924319873" @?= "52432133"
-    -- Disabled because part2' as written is far too slow.
-    -- , testCase "part2'" $ do
-    --     part2' "80871224585914546619083218645595" @?= "84462026"
-    --     part2' "19617804207202209144916044189917" @?= "78725270"
-    --     part2' "69317163492948606335995924319873" @?= "53553731"
+    , testCase "part2'" $ do
+        part2' "03036732577212944063491565474664" @?= "84462026"
+        part2' "02935109699940807407585447034323" @?= "78725270"
+        part2' "03081770884921959731165446850517" @?= "53553731"
     , testCase "part1" $ do
         p1 <- part1
         p1 @?= "94935919"
+    , testCase "part2" $ do
+        p2 <- part2
+        p2 @?= "24158285"
     ]
