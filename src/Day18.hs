4fab4461e8f7f7527dd6f8d27fe91787831e4597
Callum Oakley <c.oakley108@gmail.com>
2019-12-19 01:26:03 +0000

    wip: splitting steps by robot

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE NamedFieldPuns  #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Day18 where
 
 import           Data.Char
-import           Data.Map.Strict  (Map, (!))
+import           Data.Foldable
+import           Data.Map.Strict  (Map)
 import qualified Data.Map.Strict  as Map
+import           Data.Maybe
 import           Data.Sequence    (Seq ((:<|)), (><))
 import qualified Data.Sequence    as Seq
 import           Data.Set         (Set)
 import qualified Data.Set         as Set
+import           Data.Vector      (Vector, (//))
+import qualified Data.Vector      as Vector
+import           Debug.Trace
 import           Linear.V2
 import           Test.Tasty
 import           Test.Tasty.HUnit
@@ -22,11 +28,18 @@ data Tile
 
 type Maze = Map (V2 Int) Tile
 
-data Avatar =
-  Avatar
+data Robot =
+  Robot
     { position :: V2 Int
-    , keys     :: Set Char
     , steps    :: Int
+    }
+  deriving (Show)
+
+data State =
+  State
+    { robots   :: Vector (Robot)
+    , keys     :: Set Char
+    , lastMove :: Maybe Robot
     }
   deriving (Show)
 
@@ -34,14 +47,8 @@ isKey :: Tile -> Bool
 isKey (Key _) = True
 isKey _       = False
 
--- Consider avatars equal for the purpose of the bredth first search if they
--- have the same representation.
-represent :: Avatar -> (V2 Int, Set Char)
-represent Avatar {..} = (position, keys)
-
-findTile :: Maze -> Tile -> V2 Int
-findTile m tile =
-  fst . head . filter (\(_, tile') -> tile' == tile) $ Map.assocs m
+findTiles :: Maze -> (Tile -> Bool) -> [V2 Int]
+findTiles m p = map fst . filter (\(_, tile) -> p tile) $ Map.assocs m
 
 parse :: String -> Maze
 parse = Map.fromList . concatMap parseLine . zip [0 ..] . lines
@@ -55,47 +62,88 @@ parse = Map.fromList . concatMap parseLine . zip [0 ..] . lines
       | isLower c = Key c
       | isUpper c = Door $ toLower c
 
-bredthFirst :: Ord r => (a -> r) -> (a -> [a]) -> a -> [a]
-bredthFirst rep step start = go Set.empty (Seq.singleton start)
+breadthFirst :: (State -> [State]) -> State -> [State]
+breadthFirst step start = go Map.empty (Seq.singleton start)
   where
     go _ Seq.Empty = []
-    go seen (x :<| xs)
-      | Set.member (rep x) seen = go seen xs
-      | otherwise =
-        x : go (Set.insert (rep x) seen) (xs >< Seq.fromList (step x))
+    go seen (x :<| xs) =
+      x :
+      go
+        (Map.union
+           (Map.fromList $
+            map
+              (\State {..} ->
+                 ( (position $ fromJust lastMove, keys)
+                 , steps $ fromJust lastMove))
+              ys)
+           seen)
+        (xs >< Seq.fromList ys)
+      where
+        ys =
+          filter
+            (\State {..} ->
+               isJust lastMove &&
+               (Map.notMember (position $ fromJust lastMove, keys) seen ||
+                seen Map.! (position $ fromJust lastMove, keys) >
+                steps (fromJust lastMove))) $
+          step x
 
-moves :: Maze -> Avatar -> [Avatar]
-moves maze Avatar {..} =
-  [ Avatar {position = position', keys = keys', steps = steps + 1}
-  | dir <- [V2 1 0, V2 0 1, V2 (-1) 0, V2 0 (-1)]
-  , let position' = position + dir
-  , Map.member position' maze
-  , let tile = maze ! position'
+moves :: Maze -> State -> [State]
+moves maze State {..} =
+  [ State
+    {robots = robots // [(i, robot')], keys = keys', lastMove = Just robot'}
+  | (i, robot) <- toList $ Vector.indexed robots
+  , dir <- [V2 1 0, V2 0 1, V2 (-1) 0, V2 0 (-1)]
+  , let robot' =
+          Robot {position = position robot + dir, steps = steps robot + 1}
+  , Map.member (position robot') maze
+  , let tile = maze Map.! (position robot')
   , case tile of
-      Door c -> Set.member c keys
-      _      -> True
+      Door key -> Set.member key keys
+      _        -> True
   , let keys' =
           case tile of
-            Key c -> Set.insert c keys
-            _     -> keys
+            Key key -> Set.insert key keys
+            _       -> keys
   ]
 
-part1' :: String -> Int
-part1' s =
-  steps . head . filter (\Avatar {..} -> Set.size keys == nKeys) $
-  bredthFirst
-    represent
+fewestSteps :: Maze -> Int
+fewestSteps maze =
+  sum .
+  fmap steps . robots . head . filter (\State {..} -> Set.size keys == nKeys) $
+  breadthFirst
     (moves maze)
-    (Avatar {position = findTile maze Entrance, keys = Set.empty, steps = 0})
+    (State
+       { robots =
+           Vector.fromList . map (\position -> Robot {position, steps = 0}) $
+           findTiles maze (== Entrance)
+       , keys = Set.empty
+       , lastMove = Nothing
+       })
+  where
+    nKeys = Map.size $ Map.filter isKey maze
+
+part1' :: String -> Int
+part1' = fewestSteps . parse
+
+part2' :: String -> Int
+part2' s = fewestSteps $ Map.mapMaybeWithKey f maze
   where
     maze = parse s
-    nKeys = Map.size $ Map.filter isKey maze
+    entrance = head $ findTiles maze (== Entrance)
+    f pos tile
+      | sum (fmap abs $ pos - entrance) <= 1 = Nothing
+      | fmap abs (pos - entrance) == V2 1 1 = Just Entrance
+      | otherwise = Just tile
 
 part1 :: IO Int
 part1 = part1' <$> readFile "data/input18"
 
-testData :: [String]
-testData =
+part2 :: IO Int
+part2 = part2' <$> readFile "data/input18"
+
+testData1 :: [String]
+testData1 =
   map
     unlines
     [ ["#########", "#b.A.@.a#", "#########"]
@@ -130,6 +178,46 @@ testData =
       ]
     ]
 
+testData2 :: [String]
+testData2 =
+  map
+    unlines
+    [ [ "#######"
+      , "#a.#Cd#"
+      , "##...##"
+      , "##.@.##"
+      , "##...##"
+      , "#cB#Ab#"
+      , "#######"
+      ]
+    , [ "###############"
+      , "#d.ABC.#.....a#"
+      , "######...######"
+      , "######.@.######"
+      , "######...######"
+      , "#b.....#.....c#"
+      , "###############"
+      ]
+    , [ "#############"
+      , "#DcBa.#.GhKl#"
+      , "#.###...#I###"
+      , "#e#d#.@.#j#k#"
+      , "###C#...###J#"
+      , "#fEbA.#.FgHi#"
+      , "#############"
+      ]
+    , [ "#############"
+      , "#g#f.D#..h#l#"
+      , "#F###e#E###.#"
+      , "#dCba...BcIJ#"
+      , "#####.@.#####"
+      , "#nK.L...G...#"
+      , "#M###N#H###.#"
+      , "#o#m..#i#jk.#"
+      , "#############"
+      ]
+    ]
+
 test :: IO ()
 test = defaultMain tests
 
@@ -140,8 +228,10 @@ tests =
     [ testCase "part1'" . sequence_ $
       zipWith
         (\s expected -> part1' s @?= expected)
-        testData
+        testData1
         [8, 86, 132, 136, 81]
+    , testCase "part2'" . sequence_ $
+      zipWith (\s expected -> part2' s @?= expected) testData2 [8, 24, 32, 72]
     , testCase "part1" $ do
         p1 <- part1
         p1 @?= 3546
