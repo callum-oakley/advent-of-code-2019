80bd0a8f09a75016e99fd7c63187afb9672e0b66
Callum Oakley <c.oakley108@gmail.com>
2019-12-21 12:32:22 +0000

    day 21 part2

--- src/Day21.hs
+++ src/Day21.hs
@@ -13,6 +13,22 @@ part1 = do
     unlines
       ["OR A T", "AND B T", "AND C T", "NOT T T", "AND D T", "OR T J", "WALK"]
 
+part2 :: IO Int
+part2 = do
+  p <- Intcode.parse <$> readFile "data/input21"
+  pure . last . Intcode.run p . map ord $
+    unlines
+      [ "OR A T"
+      , "AND B T"
+      , "AND C T"
+      , "NOT T T"
+      , "AND D T"
+      , "OR E J"
+      , "OR H J"
+      , "AND T J"
+      , "RUN"
+      ]
+
 test :: IO ()
 test = defaultMain tests
 
@@ -23,4 +39,7 @@ tests =
     [ testCase "part1" $ do
         p1 <- part1
         p1 @?= 19353692
+    , testCase "part2" $ do
+        p2 <- part2
+        p2 @?= 1142048514
     ]
