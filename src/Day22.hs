cc2696bf5b1d82ed414e6b02404ee6145c2e2200
Callum Oakley <c.oakley108@gmail.com>
2019-12-23 23:41:35 +0000

    shuffle some things around

--- src/Day22.hs
+++ src/Day22.hs
@@ -6,46 +6,33 @@
 module Day22 where
 
 import           Control.Monad
-import           Data.Either
-import           Data.Functor
 import           Data.Group
 import           Data.List
 import           Data.Maybe
 import           Data.Proxy
-import           Data.Ratio
-import           GHC.TypeLits         hiding (Mod)
+import           GHC.TypeLits     hiding (Mod)
 import           Test.Tasty
 import           Test.Tasty.HUnit
-import           Text.Parsec          (Parsec, (<|>))
-import qualified Text.Parsec          as Parsec
-import qualified Text.Parsec.Language as Parsec
-import qualified Text.Parsec.Token    as Parsec
 
 newtype Mod (m :: Nat) =
   Mod Integer
   deriving (Show, Eq)
-
-unMod :: Mod (m :: Nat) -> Integer
-unMod (Mod n) = n
 
 instance forall m. KnownNat m => Num (Mod m) where
   fromInteger x = Mod $ x `mod` natVal (Proxy :: Proxy m)
   (Mod x) + (Mod y) = fromInteger $ x + y
   (Mod x) * (Mod y) = fromInteger $ x * y
   (Mod x) - (Mod y) = fromInteger $ x - y
-  abs = id
-  signum = const 1
+  abs = undefined
+  signum = undefined
 
 instance KnownNat m => Fractional (Mod m) where
   recip x = x ^ (natVal x - 2) -- True for prime m by Fermat's little theorem
-  fromRational x = fromInteger (numerator x) / fromInteger (denominator x)
+  fromRational = undefined
 
 data Shuffle (m :: Nat) =
   Shuffle (Mod m) (Mod m) -- Shuffle a b represents a linear function \x -> a x + b
   deriving (Show)
-
-base :: KnownNat m => Shuffle m -> Integer
-base (Shuffle a _) = natVal a
 
 instance KnownNat m => Semigroup (Shuffle m) where
   (Shuffle a b) <> (Shuffle c d) = Shuffle (c * a) (c * b + d)
@@ -53,33 +40,19 @@ instance KnownNat m => Semigroup (Shuffle m) where
 instance KnownNat m => Monoid (Shuffle m) where
   mempty = Shuffle 1 0
 
+-- Shuffle m forms a group under composition as long as m is prime (so that Mod
+-- m is Fractional, see above.)
 instance KnownNat m => Group (Shuffle m) where
   invert (Shuffle a b) = Shuffle (1 / a) (-b / a)
 
-data Technique
-  = DealIntoNewStack
-  | Cut Integer
-  | DealWithIncrement Integer
-  deriving (Show)
-
-parseTechnique :: Parsec String () Technique
-parseTechnique =
-  Parsec.string "cut " *> (Cut <$> integer) <|>
-  Parsec.string "deal " *>
-  (Parsec.string "into new stack" $> DealIntoNewStack <|>
-   Parsec.string "with increment " *> (DealWithIncrement <$> integer))
-  where
-    integer = Parsec.integer Parsec.haskell
-
-fromTechnique :: KnownNat m => Technique -> Shuffle m
-fromTechnique DealIntoNewStack      = Shuffle (-1) (-1)
-fromTechnique (Cut n)               = Shuffle 1 (-fromInteger n)
-fromTechnique (DealWithIncrement n) = Shuffle (fromInteger n) 0
-
 parse :: KnownNat m => String -> Shuffle m
-parse =
-  mconcat .
-  map fromTechnique . rights . map (Parsec.parse parseTechnique "") . lines
+parse = mconcat . map parseTechnique . lines
+  where
+    parseTechnique s =
+      case words s of
+        ["deal", "into", "new", "stack"] -> Shuffle (-1) (-1)
+        ["cut", n] -> Shuffle 1 (-(fromInteger $ read n))
+        ["deal", "with", "increment", n] -> Shuffle (fromInteger $ read n) 0
 
 apply :: KnownNat m => Shuffle m -> Mod m -> Mod m
 apply (Shuffle a b) card = a * card + b
@@ -102,6 +75,7 @@ simulateFullShuffle shuffle =
     [0 .. base shuffle - 1]
   where
     positions = map (apply shuffle . fromInteger) [0 .. base shuffle - 1]
+    base (Shuffle a _) = natVal a
 
 testData :: [String]
 testData =
